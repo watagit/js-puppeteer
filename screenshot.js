cee91bf0afec7e7ce66375848c97ab8ab83a8ade
Wataru Ono <wtr0414@gmail.com>
2020-12-06 17:30:25 +0900

    :lollipop: インデントを変更

--- screenshot.js
+++ screenshot.js
@@ -1,9 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto('http://example.com/');
-	await page.screenshot({ path: 'screenshot.png' });
-	await browser.close();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://example.com/');
+  await page.screenshot({ path: 'screenshot.png' });
+  await browser.close();
 })();
